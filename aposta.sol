dac08a1971bdee74fbec2b8dcd314e4db048c34d
victorgxbriel <victorgabrielcn@hotmail.com>
2023-12-11 18:54:19 -0300

    first commit

---
+++ aposta.sol
@@ -0,0 +1,11 @@
+// Nome: Victor Gabriel Araujo Silva
+// Nome: Israel Hall Leigthon
+// Conta do contrato: 0x63EDa3652a2067e881EE1Adce9b5949b4D0e38c3
+
+// Seu contrato começa aqui!
+  // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Aposta {
+    
+}
